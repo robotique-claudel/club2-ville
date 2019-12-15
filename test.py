529c9571587b36c442da1403649b5b902cf3de63
tommcn <42918822+tommcn@users.noreply.github.com>
2019-12-15 17:10:03 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
 import keras
-from keras import backend as K
-from keras.layers.core import Dense, Activation
-from keras.optimizers import Adam
-from keras.metrics import categorical_crossentropy
-from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
-from keras.models import Model
 from keras.applications import imagenet_utils
-from keras.layers import Dense,GlobalAveragePooling2D
-from keras.applications import MobileNet
 from keras.applications.mobilenet import preprocess_input
 import numpy as np
-#from IPython.display import Image
-from keras.optimizers import Adam
+from os import system
+from time import sleep
 
 mobile = keras.applications.mobilenet.MobileNet()
 
@@ -21,12 +13,15 @@ def prepare_image(file):
     img = image.load_img(img_path + file, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
-    return keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
+    return preprocess_input(img_array_expanded_dims)
 
-preprocessed_image = prepare_image('download.jpg')
-predictions = mobile.predict(preprocessed_image)
-results = imagenet_utils.decode_predictions(predictions)
+def predict(path):
+    preprocessed_image = prepare_image(path)
+    predictions = mobile.predict(preprocessed_image)
+    results = imagenet_utils.decode_predictions(predictions)
+    print(results)
 
-print(results)
-
-
+while True:
+    system("raspistill -o output.jpg")
+    predict("output.jpg")
+    sleep(2)
