a6f361a5cf19173c9e2ef842367aa97bae89c928
tommcn <42918822+tommcn@users.noreply.github.com>
2020-01-16 13:08:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,15 +5,11 @@ GPIO.setmode(GPIO.BCM)
 PIR_PIN = 7
 GPIO.setup(PIR_PIN, GPIO.IN)
 
-try:
-               print("PIR Module Test (CTRL+C to exit)")
-               time.sleep(2)
-               print("Ready”)
-               while True:
-                             if GPIO.input(PIR_PIN):
-                                             print("Motion Detected!")
-                              time.sleep(1)
-except KeyboardInterrupt:
-               print “ Quit”
-               GPIO.cleanup()
+print("PIR Module Test (CTRL+C to exit)")
+time.sleep(2)
+print("Ready")
+while True:
+             if GPIO.input(PIR_PIN):
+                             print("Motion Detected!")
+              time.sleep(1)
 
