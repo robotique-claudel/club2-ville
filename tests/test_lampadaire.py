75e9b562e4bbde409b1f084b38962a8b8432533d
tommcn <tommcn@mcnamer.ca>
2020-07-01 12:32:15 -0400

    meilluer tests

--- tests/test_lampadaire.py
+++ tests/test_lampadaire.py
@@ -5,16 +5,10 @@ sys.path.insert(0,'..')
 # pylint: disable=import-error
 import composant
 
-class TestSum(unittest.TestCase):
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 3)), 6, "Should be 6")
-
 class TestLampadaire(unittest.TestCase):
     def test_allume(self):
         l = composant.Lampadaire(1)
+        self.assertTrue(isinstance(l, composant.Lampadaire))
 
 
 if __name__ == '__main__':
