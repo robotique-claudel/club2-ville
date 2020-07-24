7ff784e552a86e51fffc004b3fe6578dc96f1dc4
tommcn <tommcn@users.noreply.github.com>
2020-07-24 23:42:10 +0000

    deploy: e3dae98b4eb22e5d884d98fe4499218608bb9755

--- menudata.js
+++ menudata.js
@@ -24,7 +24,6 @@
 */
 var menudata={children:[
 {text:"Page principale",url:"index.html"},
-{text:"Pages associées",url:"pages.html"},
 {text:"Classes",url:"annotated.html",children:[
 {text:"Liste des classes",url:"annotated.html"},
 {text:"Index des classes",url:"classes.html"},
